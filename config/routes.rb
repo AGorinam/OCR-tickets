1dc791998f51a6aaecc4a2e41841582eafd5bd0c
Alejandro Gorina <alejandrogorinam@gmail.com>
2023-09-12 22:19:31 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,13 @@
 Rails.application.routes.draw do
-  get 'consumptions/new'
-  get 'consumptions/create'
-  get 'consumptions/edit'
-  get 'consumptions/update'
-  get 'consumptions/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'tickets/new'
-  get 'tickets/create'
-  get 'tickets/show'
-  get 'tickets/index'
-  get 'tickets/destroy'
-  get 'groups/index'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/show'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'groups#index'
+
+  resources :groups do
+    resources :tickets, only: %i[new create show index destroy] do
+      resources :products, only: %i[index show]
+    end
+  end
+
+  resources :consumptions, only: %i[new create edit update destroy]
 end
